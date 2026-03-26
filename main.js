4d3135b6491bd8af24c434a149347b5b008ac9fe
chumbaandrew <sharonaafa36@gmail.com>
2026-03-27 02:22:55 +0300

    landing page final

---
+++ main.js
@@ -0,0 +1,17 @@
+function handlealert(){
+    alert("Edit this contact?");
+}
+
+function handledelete(){
+    let confirmdelete = confirm("Do you want to delete this contact ?");
+
+    if(confirmdelete){
+        alert("Deleted successfully !")
+    }
+    else{
+        alert("Wagwan noted !")
+    }
+}
+function soon(){
+    alert("This button doesnt function at the moment, coming soon!");
+}
